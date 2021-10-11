e72e0d9c0e4bb4710ecc97c2aa9c7fca19e8f436
dancrux <nwogwugwudaniel@gmail.com>
2021-10-11 17:23:56 +0100

    Added answer for how for loops work

--- src/Question3.java
+++ src/Question3.java
@@ -16,14 +16,12 @@ public class Question3 {
 
     }
 
-
-
 //        Question 3b
 
        /*i. for (x=1; x<=10; x++) { } Executes 10 times
 ii. for (x=0; x>10; x++) { } Does not execute Because the condition returns false, because initial value of x is less than 10
 iii. for (x=10; x>=0; x--) { } Executes 11 times
-iv. for (x=-10; x<=10; x++) { } Executes 21 times*/
+iv. for (x=-10; x<=10; x++) { } Executes 21 times */
 
 //       Question 3c
         /*
